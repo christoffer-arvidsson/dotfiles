80bf9ccf88b6cb190b7cc05a23d1c7c10bb3038b
Christoffer Arvidsson <christoffer@arvidson.nu>
2020-04-02 23:13:18 +0200

    Add source to corona tracker

--- bspwm/.config/polybar/scripts/corona/get_sweden_daily.sh
+++ bspwm/.config/polybar/scripts/corona/get_sweden_daily.sh
@@ -4,13 +4,14 @@ COUNTRY="SE"
 API="https://corona-stats.online"
 MINIMAL="true"
 JSON="false"
+SOURCE=2
 
-aqi=$(curl -sf "$API/$COUNTRY?minimal=$MINIMAL&json=$JSON" | grep Sweden)
+aqi=$(curl -sf "$API/$COUNTRY?minimal=$MINIMAL&json=$JSON&source=$Source" | grep Sweden)
 COLORLESS=$(sed 's/\x1b\[[0-9;]*m//g' <<< $aqi)
 
 FIELDS=(${COLORLESS})
-ACTIVE=${FIELDS[5]}
-INCREASE=${FIELDS[8]}
+ACTIVE=${FIELDS[10]}
+INCREASE=${FIELDS[4]}
 
 if [ "$INCREASE" -gt 0 ]; then
     ICON="%{F#e60053}"
