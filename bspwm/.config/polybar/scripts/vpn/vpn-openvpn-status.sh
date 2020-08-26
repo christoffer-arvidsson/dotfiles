95c9b3dc2987f06434e57906e8ef21b90ea1e6a8
Christoffer Arvidsson <christoffer@arvidson.nu>
2020-08-26 21:30:48 +0200

    Make vpn widget use ip addr rather than ifconfig.

--- bspwm/.config/polybar/scripts/vpn/vpn-openvpn-status.sh
+++ bspwm/.config/polybar/scripts/vpn/vpn-openvpn-status.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 
-IFACE=$(ip addr | grep tun0 | awk '{print $1}')
+IFACE=$(ip addr | grep tun0: | awk '{print $2}')
 
 if [ "$IFACE" = "tun0:" ]; then
-    echo " %{F#4fb069} $(ifconfig tun0 | grep inet | awk '{print $2}' | cut -f2 -d ':')%{u-}"
+    echo " %{F#4fb069} $(ip addr show tun0 | grep inet | awk '{print $2}' | cut -f2 -d ':' | cut -c -12)%{u-}"
 else
     echo "%{F#E75D5D} no vpn"
 fi
