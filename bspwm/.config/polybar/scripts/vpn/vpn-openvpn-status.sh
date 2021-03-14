d819961fd8da0b3beccfbd02adb11aab19033681
Christoffer Arvidsson <christoffer@arvidson.nu>
2021-03-14 16:50:10 +0100

    Fix vpn widget

--- bspwm/.config/polybar/scripts/vpn/vpn-openvpn-status.sh
+++ bspwm/.config/polybar/scripts/vpn/vpn-openvpn-status.sh
@@ -4,7 +4,7 @@
 IFACE=$(ip addr | grep tun0: | awk '{print $2}')
 
 if [ "$IFACE" = "tun0:" ]; then
-    echo " %{F#8fc99f} $(ip addr show tun0 | grep inet | awk '{print $2}' | cut -f2 -d ':' | cut -c -12)%{u-}"
+    echo " %{F#8fc99f} $(ip addr show tun0 | grep inet | awk '{print $2}' | cut -f2 -d ':' | cut -c -13)%{u-}"
 else
     echo "%{F#E75D5D} no vpn"
 fi
