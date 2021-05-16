54a278620fb098bc5b1b737f9e2073b2fb4cc071
Christoffer Arvidsson <christoffer@arvidson.nu>
2021-05-16 11:29:44 +0200

    Filter mails to avoid gmails stupid mail duplication

--- bspwm/.config/polybar/scripts/mail/num_unread.sh
+++ bspwm/.config/polybar/scripts/mail/num_unread.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-NUM_UNREAD=$(mu msgs-count --query='flag:unread')
+# Because gmail is weird, they serve "labels" where a mail can belong to
+# multiple such labels. This query disregards certain mailboxes to not
+# duplicate unread mails.
+NUM_UNREAD=$(mu msgs-count --query='flag:unread AND NOT maildir:/gmail/[Gmail]/Bin AND NOT maildir:/gmail/[Gmail]/Spam AND NOT maildir:/gmail/[Gmail]/Sent Mail')
 
 # echo "No unread mails"
 if [ "$NUM_UNREAD" = "0" ]; then
