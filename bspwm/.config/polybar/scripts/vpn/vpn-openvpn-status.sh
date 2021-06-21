e8ade023d7e231c5cb6a338941391e09e4574058
Christoffer Arvidsson <christoffer@arvidson.nu>
2021-06-21 17:33:44 +0200

    Fix vpn script again

--- bspwm/.config/polybar/scripts/vpn/vpn-openvpn-status.sh
+++ bspwm/.config/polybar/scripts/vpn/vpn-openvpn-status.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 
-IFACE=$(ip addr | grep wg-mullvad: | awk '{print $2}')
+IFACE=$(ip addr | grep tun0: | awk '{print $2}')
 
-if [ "$IFACE" = "wg-mullvad:" ]; then
-    echo " %{F#8fc99f} $(ip addr show wg-mullvad | grep inet | awk '{print $2}' | cut -f2 -d ':' | cut -c -13)%{u-}"
+if [ "$IFACE" = "tun0:" ]; then
+    echo " %{F#8fc99f} $(ip addr show tun0 | grep "inet " | awk '{print $2}' | cut -f2 -d ':' | cut -c -13)%{u-}"
 else
-    echo "%{F#E75D5D} no vpn"
+    echo "%{F#E75D5D} no vpn"
 fi
