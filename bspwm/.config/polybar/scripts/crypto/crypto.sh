b5ab72c9be5ffd033f2c9fda27291d3010a457e7
Christoffer Arvidsson <christoffer@arvidson.nu>
2021-06-26 23:45:32 +0200

    Clean repo of old files

--- bspwm/.config/polybar/scripts/crypto/crypto.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-API="https://api.kraken.com/0/public/Ticker"
-
-quote=$(curl -sf $API?pair=ETHUSD | jq -r ".result.XETHZUSD.c[0]")
-quote=$(LANG=C printf "%.2f" "$quote")
-
-echo "\$$quote"
