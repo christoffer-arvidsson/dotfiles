f014523fda505e94798922d9455f1b5b1deec891
Christoffer Arvidsson <christoffer@arvidson.nu>
2022-01-17 20:18:33 +0100

    Add pomodoro script

--- bspwm/.config/polybar/scripts/emacs-pomodoro/pomodoro_bar.sh
+++ bspwm/.config/polybar/scripts/emacs-pomodoro/pomodoro_bar.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
+function emacs_server_is_running()
+{
+    ps -ef | grep [e]macsserver > /dev/null
+}
 
 underline_color="%{u#99c2ff}%{+u}"
-pomo_message=$(emacsclient -s nondoom -e '(eethern/org-pomodoro-time)' | cut -d '"' -f 2)
+
+if ! [ emacs_server_is_running ]; then
+    pomo_message=$(emacsclient -s nondoom -e '(eethern/org-pomodoro-time)' | cut -d '"' -f 2)
+else
+    pomo_message=""
+fi
+
 echo ${underline_color}${pomo_message}
