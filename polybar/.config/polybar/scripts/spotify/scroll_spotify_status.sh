905bb81a6f03b4dd2e97fff66f0214c22fca12b5
Christoffer Arvidsson <christoffer@arvidson.nu>
2022-04-17 11:20:22 +0200

    Minor polybar changes

--- polybar/.config/polybar/scripts/spotify/scroll_spotify_status.sh
+++ polybar/.config/polybar/scripts/spotify/scroll_spotify_status.sh
@@ -2,7 +2,7 @@
 
 # see man zscroll for documentation of the following parameters
 zscroll -l 30 \
-        --delay 0.1 \
+        --delay 0.3 \
         --scroll-padding " | " \
         --match-command "`dirname $0`/get_spotify_status.sh --status" \
         --match-text "Playing" "--scroll 1" \
