cbde12b5eec326d9880d7650c3f0af738a3af4c1
Christoffer Arvidsson <christoffer.arvidsson@zenseact.com>
2022-08-11 17:45:29 +0200

    Silence clock widget if emacs is not found

--- polybar/.config/polybar/scripts/emacs-clock/emacs_clock.sh
+++ polybar/.config/polybar/scripts/emacs-clock/emacs_clock.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-is_running=$(emacsclient -s nondoom -a false -e 't')
-underline_color="%{u#99c2ff}%{+u}"
 
-if [ $is_running ]; then
-    minutes="$(emacsclient -s nondoom -e '(eethern/org-clocking-info)' | cut -d '"' -f 2)"
-else
-    minutes=""
+user=`id -u`
+if test ! -e "/run/user/$user/emacs/nondoom"; then
+    exit
 fi
 
+underline_color="%{u#99c2ff}%{+u}"
+minutes=`emacsclient -s nondoom -e '(eethern/org-clocking-info)' -a false | cut -d '"' -f 2`
+
 echo "${underline_color}${minutes}"
+
