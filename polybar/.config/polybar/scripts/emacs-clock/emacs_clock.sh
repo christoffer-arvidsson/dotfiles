dd05e9a0d14ed0d5cab0b74f6e41494f1ffe393a
Christoffer Arvidsson <christoffer@arvidson.nu>
2022-08-13 09:24:19 +0200

    Make emacs-clock display task heading
    It also zscrolls the output :)

--- polybar/.config/polybar/scripts/emacs-clock/emacs_clock.sh
+++ polybar/.config/polybar/scripts/emacs-clock/emacs_clock.sh
@@ -5,10 +5,9 @@ is_running=$(ps auxw | grep "\-\-daemon=nondoom")
 if [ -z "$is_running" ]; then
     echo ""
 else
-    underline_color="%{u#99c2ff}%{+u}"
     minutes=`emacsclient -s nondoom -e '(eethern/org-clocking-info)' -a false | cut -d '"' -f 2`
 
-    echo "${underline_color}${minutes}"
+    echo "$minutes"
 fi
 
 
