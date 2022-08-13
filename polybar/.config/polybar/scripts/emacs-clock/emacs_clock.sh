be7067cdaec60ce976646d766939cd52d33f0e0a
Christoffer Arvidsson <christoffer@arvidson.nu>
2022-08-13 09:00:47 +0200

    Finally fix the clock widget

--- polybar/.config/polybar/scripts/emacs-clock/emacs_clock.sh
+++ polybar/.config/polybar/scripts/emacs-clock/emacs_clock.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-user=`id -u`
-if test ! -e "/run/user/$user/emacs/nondoom"; then
-    exit
+is_running=$(ps auxw | grep "\-\-daemon=nondoom")
+
+if [ -z "$is_running" ]; then
+    echo ""
+else
+    underline_color="%{u#99c2ff}%{+u}"
+    minutes=`emacsclient -s nondoom -e '(eethern/org-clocking-info)' -a false | cut -d '"' -f 2`
+
+    echo "${underline_color}${minutes}"
 fi
 
-underline_color="%{u#99c2ff}%{+u}"
-minutes=`emacsclient -s nondoom -e '(eethern/org-clocking-info)' -a false | cut -d '"' -f 2`
-
-echo "${underline_color}${minutes}"
 
