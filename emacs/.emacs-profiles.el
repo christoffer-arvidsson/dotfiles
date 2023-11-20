802e4c0c21b36df8284cb6d43ebc4cb3775ba992
Christoffer Arvidsson <christoffer@arvidson.nu>
2023-11-20 21:33:22 +0100

    Readd null emacs profile

--- emacs/.emacs-profiles.el
+++ emacs/.emacs-profiles.el
@@ -1,2 +1,4 @@
 (("default" . ((user-emacs-directory . "~/.config/null_emacs")
-               (server-name . "null_server"))))
+               (server-name . "null_server")))
+ ("null" . ((user-emacs-directory . "~/.config/null_emacs")
+            (server-name . "null_server"))))
