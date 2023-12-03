cc10d50689e1fc028b1801c7dc2e0e7b97ad0a9e
Christoffer Arvidsson <christoffer@arvidson.nu>
2023-12-03 21:09:31 +0100

    Tweak spotify scroll widget

--- polybar/.config/polybar/scripts/spotify/scroll_spotify_status.sh
+++ polybar/.config/polybar/scripts/spotify/scroll_spotify_status.sh
@@ -3,7 +3,7 @@
 # see man zscroll for documentation of the following parameters
 zscroll -l 30 \
         --delay 0.3 \
-        --before-text "  " \
+        --before-text "  " \
         --scroll-padding " | " \
         --match-command "`dirname $0`/get_spotify_status.sh --status" \
         --match-text "Playing" "--scroll 1" \
