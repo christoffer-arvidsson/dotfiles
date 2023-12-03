814c05af797966294bdce3232c8a013d6e8e474c
Christoffer Arvidsson <christoffer@arvidson.nu>
2023-12-03 21:09:18 +0100

    Add nice bspwm opened windows widget

---
+++ polybar/.config/polybar/scripts/bspwm/bspwm_windows.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# Define an associative array for program icons
+declare -A program_icons
+program_icons["firefox"]=""
+program_icons["firefox-bin"]=""
+program_icons["code"]=""
+program_icons["emacs"]=""
+program_icons["terminal"]=""
+program_icons["kitty"]=""
+program_icons["qutebrowser"]="qb"
+program_icons["DiscordCanary"]=""
+program_icons["slack"]=""
+program_icons["spotify"]=""
+program_icons["zathura"]=""
+
+# Get the node IDs of all windows in the focused desktop
+nodes=$(bspc query -N -n .window -d focused -m focused)
+
+# Get the focused window ID
+focused_window=$(bspc query -N -n focused)
+
+if [ -n "$focused_window" ]; then
+    # Get the PID of the currently focused window
+    current_pid=$(xprop -id "$focused_window" _NET_WM_PID | awk '{print $3}')
+    current_program=$(ps -o comm= -p "$current_pid")
+else
+    current_program="No focused window"
+    echo ""
+    exit 0
+fi
+
+# Formatting
+b_sel="%{+u}%{u#e1986d}"
+e_sel="%{-u}"
+
+# Loop over each node to get program names
+echo "$nodes" | while IFS= read -r node; do
+    # Get the PID of the current node
+    pid=$(xprop -id "$node" _NET_WM_PID | awk '{print $3}')
+
+    if [ -n "$pid" ]; then
+        # Get the program name associated with the PID
+        program=$(ps -o comm= -p "$pid")
+
+        # Check if the current node corresponds to the focused window
+        if [ "$pid" = "$current_pid" ]; then
+            # Print the program name with a marker and icon to indicate the currently focused window
+            echo -n " $b_sel${program_icons[$program]:-$program}$e_sel"
+        else
+            # Print the program name with an icon if available, otherwise just the program name
+            echo -n " ${program_icons[$program]:-$program}"
+        fi
+        echo -n " "
+    fi
+done
+
+# Print a message if there is no focused window
+if [ "$current_program" = "No focused window" ]; then
+    echo " $current_program"
+fi
--- polybar/.config/polybar/scripts/bspwm/curr_windows.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-nodes=$(bspc query -N -n .window -d focused)
-titles=$(xtitle $nodes)
-
-current=$(bspc query -N -n focused)
-
-echo $titles
