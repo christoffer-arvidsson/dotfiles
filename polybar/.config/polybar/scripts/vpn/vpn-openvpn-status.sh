5c0b210676b3fda5ce20cc8165640cf5189320b7
Christoffer Arvidsson <christoffer@arvidson.nu>
2023-12-03 19:45:05 +0100

    Tweak icons on home computer

--- polybar/.config/polybar/scripts/vpn/vpn-openvpn-status.sh
+++ polybar/.config/polybar/scripts/vpn/vpn-openvpn-status.sh
@@ -3,7 +3,7 @@
 IFACE=$(ip addr | grep ppp0: | awk '{print $2}')
 
 if [ "$IFACE" = "ppp0:" ]; then
-    echo "%{F#8fc99f} $(ip addr show $IFACE | grep "inet " | awk '{print $2}' | cut -f2 -d ':' | cut -c -13)%{u-}"
+    echo "%{F#8fc99f} $(ip addr show $IFACE | grep "inet " | awk '{print $2}' | cut -f2 -d ':' | cut -c -13)%{u-}"
 else
-    echo "%{F#E75D5D} no vpn"
+    echo "%{F#E75D5D} vpn"
 fi
