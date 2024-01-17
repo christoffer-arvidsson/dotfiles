0a0fa87f0f7562a2cd395e66b6a4d7be448ca0eb
Christoffer Arvidsson <christoffer@arvidson.nu>
2024-01-17 18:01:44 +0100

    Add qutebrowser session workflow scripts
    Useful to do a sortof "container-tabs" thing but for the whole
    browser. Lets you maintain different profiles of logins and not share
    history or quickmarks between them.

---
+++ qutebrowser/.config/scripts/qutebrowser-session.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+xdg_config_home=${XDG_CONFIG_HOME:-~/.config}
+profiles_dir="$xdg_config_home/qutebrowser/profiles/"
+qb_config="$xdg_config_home/qutebrowser/config.py"
+
+function get_profiles() {
+    find "$profiles_dir" -mindepth 1 -maxdepth 1 -type d
+}
+
+function select_profile() {
+    rofi -dmenu -no-custom -i -p "Select qutebrowser session" $1
+}
+
+selected=`get_profiles | select_profile`
+
+if [[ -n "$selected" ]]; then
+    qutebrowser -B $selected -C $qb_config &
+fi
