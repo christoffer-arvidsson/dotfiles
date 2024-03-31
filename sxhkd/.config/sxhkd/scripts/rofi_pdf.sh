8f27fd901617fa1026fe59b158118ae356ff636d
Christoffer Arvidsson <christoffer@arvidson.nu>
2024-03-31 16:59:51 +0200

    [sxhkd+rofi] add rofi-pdf

---
+++ sxhkd/.config/sxhkd/scripts/rofi_pdf.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Simple script to open a pdf in a list of directories.
+DIRECTORIES=("$HOME/Zotero/" "$HOME/Downloads" "$HOME/Documents")
+
+find_pdfs() {
+    find ${DIRECTORIES[@]} -name "*.pdf"
+}
+
+SELECTED=$(find_pdfs | rofi -i -dmenu -p "Select a pdf")
+
+if [[ -n "$SELECTED" ]]; then
+    zathura "$SELECTED"
+fi
