5069dd1a1cc7f3707e3eb5bf4e25971ca5034b9b
Christoffer Arvidsson <christoffer@arvidson.nu>
2024-04-18 17:55:43 +0200

    Cleanup autorandr and emacs-profiles

--- emacs/.emacs-profiles.el
+++
@@ -1,4 +0,0 @@
-(("default" . ((user-emacs-directory . "~/.config/null_emacs")
-               (server-name . "null_server")))
- ("null" . ((user-emacs-directory . "~/.config/null_emacs")
-            (server-name . "null_server"))))
