cd87c286fb5ff0e2b0973b21b11b11819f2bec3d
Christoffer Arvidsson <christoffer.arvidsson@zenseact.com>
2024-10-21 22:50:58 +0200

    Don't display vpn ip in polybar

--- polybar/.config/polybar/scripts/vpn/vpn-openvpn-status.sh
+++ polybar/.config/polybar/scripts/vpn/vpn-openvpn-status.sh
@@ -3,7 +3,8 @@
 IFACE=$(ip addr | grep ppp0: | awk '{print $2}')
 
 if [ "$IFACE" = "ppp0:" ]; then
-    echo "%{F#8fc99f} $(ip addr show $IFACE | grep "inet " | awk '{print $2}' | cut -f2 -d ':' | cut -c -13)%{u-}"
+    _ip=$(ip addr show $IFACE | grep "inet " | awk '{print $2}' | cut -f2 -d ':' | cut -c -13)
+    echo "%{F#8fc99f} vpn%{u-}"
 else
-    echo "%{F#E75D5D} vpn"
+    echo "%{F#E75D5D} vpn%{u-}"
 fi
