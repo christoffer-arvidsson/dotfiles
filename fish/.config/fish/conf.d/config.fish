89621c7ae64a086937bf1b38f4c85a9a85b0ca9b
Christoffer Arvidsson <christoffer.arvidsson@zenseact.com>
2024-10-21 22:50:48 +0200

    Add some nice defaules to Feh

--- fish/.config/fish/conf.d/config.fish
+++ fish/.config/fish/conf.d/config.fish
@@ -58,6 +58,9 @@ if command -v tmux >/dev/null 2>&1
     alias ts="~/.config/scripts/tmux_sessionizer.sh"
 end
 
+# feh
+alias feh="feh --draw-filename -B 'black' --scale-down -R 5" 
+
 # pyenv
 if command -v pyenv > /dev/null 2>&1
     status --is-interactive; and pyenv init - | source
