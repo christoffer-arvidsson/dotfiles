605ce62215039aedbed1aa1d9566bacb245c0d1d
Christoffer Arvidsson <christoffer@arvidson.nu>
2025-03-09 12:34:21 +0100

    Update vpn colors

--- polybar/.config/polybar/scripts/vpn/vpn-openvpn-status.sh
+++ polybar/.config/polybar/scripts/vpn/vpn-openvpn-status.sh
@@ -2,9 +2,16 @@
 
 IFACE=$(ip addr | grep ppp0: | awk '{print $2}')
 
+function get_color {
+    xrdb -query | grep "^$1" | awk '{print $2}'
+}
+
+color_alert=$(get_color "alert")
+color_positive=$(get_color "positive")
+
 if [ "$IFACE" = "ppp0:" ]; then
     _ip=$(ip addr show $IFACE | grep "inet " | awk '{print $2}' | cut -f2 -d ':' | cut -c -13)
-    echo "%{F#8fc99f} vpn%{u-}"
+    echo "%{F$color_positive} vpn%{u-}"
 else
-    echo "%{F#E75D5D} vpn%{u-}"
+    echo "%{F$color_alert} vpn%{u-}"
 fi
