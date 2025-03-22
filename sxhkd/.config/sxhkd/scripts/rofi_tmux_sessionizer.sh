7634781c05e2e00394685f0886895e33b30044e4
Christoffer Arvidsson <christoffer.arvidsson@zenseact.com>
2025-03-22 17:27:14 +0100

    Make tmux-sessionizer case-insensitive

--- sxhkd/.config/sxhkd/scripts/rofi_tmux_sessionizer.sh
+++ sxhkd/.config/sxhkd/scripts/rofi_tmux_sessionizer.sh
@@ -5,7 +5,6 @@ source $HOME/.config/sxhkd/scripts/tmux_dirs.sh
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-
     selected=$(find "${TMUX_DIRS[@]}" -mindepth 0 -maxdepth 1 -type d | rofi -dmenu -p "Select directory for tmux session")
 fi
 
